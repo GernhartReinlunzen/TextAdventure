9c00a8dcca01775e82e673fa5ffe1dc065afcbc1
Der Jens <boy1512001@gmail.com>
2016-09-16 08:51:31 +0100

    5 jens

---
+++ src/model/Creature.java
@@ -0,0 +1,23 @@
+package Model;
+
+/**
+ * Created by 204g06 on 16.09.2016.
+ */
+public class Creature {
+    private int healthPoints;
+    private int attackDamage;
+    private int abilityPower;
+    private int initiative;
+    private int testn;
+
+    public Creature (int healthPoints, int attackDamage ,int abilityPower, int initiative){
+        this.healthPoints = healthPoints;
+        this.attackDamage = attackDamage;
+        this.abilityPower = abilityPower;
+        this.initiative = initiative;
+    }
+
+    public void bla(){
+
+    }
+}
