40d5cfd2493bb52209d52a8e3a3b1ae0851fe476
Der Jens <boy1512001@gmail.com>
2016-09-16 08:07:10 +0100

    2. Commit

---
+++ src/Control/MainController.java
@@ -0,0 +1,7 @@
+package Control;
+
+/**
+ * Created by 204g06 on 16.09.2016.
+ */
+public class MainController {
+}
---
+++ src/View/MainView.java
@@ -0,0 +1,8 @@
+package View;
+
+/**
+ * Created by 204g06 on 16.09.2016.
+ */
+public class MainView {
+
+}
