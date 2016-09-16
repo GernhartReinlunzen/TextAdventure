a36d49e14d4f7adcc77a152397159c3d822b12f4
Der Jens <boy1512001@gmail.com>
2016-09-16 09:00:40 +0100

    5 jens

--- src/model/Creature.java
+++ src/model/Creature.java
@@ -8,16 +8,53 @@ public class Creature {
     private int attackDamage;
     private int abilityPower;
     private int initiative;
-    private int testn;
 
     public Creature (int healthPoints, int attackDamage ,int abilityPower, int initiative){
         this.healthPoints = healthPoints;
         this.attackDamage = attackDamage;
         this.abilityPower = abilityPower;
         this.initiative = initiative;
+
     }
 
-    public void bla(){
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public void setAttackDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    public int getAbilityPower() {
+        return abilityPower;
+    }
+
+    public void setAbilityPower(int abilityPower) {
+        this.abilityPower = abilityPower;
+    }
+
+    public int getInitiative() {
+        return initiative;
+    }
+
+    public void setInitiative(int initiative) {
+        this.initiative = initiative;
+    }
+
+    public void angreifen(Creature enemy){
+       enemy.setHealthPoints();
+    }
+
+    public void magischerAngriff(Creature enemy){
+        enemy.healthPoints() = enemy.getHealthPoints() - this.getAbilityPower();
 
     }
 }
