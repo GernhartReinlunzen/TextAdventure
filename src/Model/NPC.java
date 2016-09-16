2ddd558009eff191737c035d9da21a58f7539089
Der Jens <boy1512001@gmail.com>
2016-09-16 08:14:47 +0100

    3. Commit

---
+++ src/Model/NPC.java
@@ -0,0 +1,11 @@
+package Model;
+
+/**
+ * Created by 204g06 on 16.09.2016.
+ */
+public abstract class Character {
+    private int healthPoints;
+    private int attackDamage;
+    private int abilityPower;
+    private int initiative;
+}
