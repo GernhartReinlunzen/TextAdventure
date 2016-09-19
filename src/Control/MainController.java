e6a1aa67259ebb1552257fb073b07177e388f51c
Noel <elliwsv@gmail.com>
2016-09-19 10:26:33 +0100

    neuste1

--- src/Control/MainController.java
+++ src/Control/MainController.java
@@ -9,4 +9,12 @@ public class MainController {
     public MainController(Creature player){
         player = new Creature("",100,5,0,5);
     }
+
+    public void TakeDmg(Creature reciever,int dmg){
+        reciever.setHealthPoints(reciever.getHealthPoints()-dmg);
+    }
+
+    public void sterben(Creature reciever){
+
+    }
 }
